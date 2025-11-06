d7e8ee2e6b78a44736a508c8cb1a7dc0ec1f2b95
Hasan Çoban <hasancoban13579@gmail.com>
2025-11-07 02:21:29 +0300

    Enhance contact management with sorting and printing functionality
    - Introduced Contact and ContactSorter types to manage and sort contacts based on XOR distance from a target NodeId.
    - Updated xorDistance function to return a big.Int for improved distance calculations.
    - Modified main function to create a list of contacts, sort them, and print the results.

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
+	"math/big"
+	"net"
+	"sort"
+	"strconv"
 )
 
 type NodeId [20]byte
@@ -12,22 +17,62 @@ func NewNodeId(name string) NodeId {
 }
 
 func (nodeId NodeId) String() string {
-	return string(nodeId[:]) // ":" is needed for arrays, it means "from beginning to end"
+	return hex.EncodeToString(nodeId[:])
 }
 
-func xorDistance(a, b NodeId) int {
-	dist := 0
-	for i := 0; i < len(a); i++ {
-		dist += int(a[i] ^ b[i])
-	}
+func xorDistance(a, b NodeId) *big.Int {
+	ai := new(big.Int).SetBytes(a[:])
+	bi := new(big.Int).SetBytes(b[:])
+	return new(big.Int).Xor(ai, bi)
+}
 
-	return dist
+type Contact struct {
+	ID   NodeId
+	IP   net.IP
+	Port int
+}
+
+type ContactSorter struct {
+	TargetID NodeId
+	Contacts []Contact
+}
+
+// sort.Interface implementation
+func (cs ContactSorter) Len() int { return len(cs.Contacts) }
+func (cs ContactSorter) Swap(i, j int) {
+	cs.Contacts[i], cs.Contacts[j] = cs.Contacts[j], cs.Contacts[i]
+}
+
+func (cs ContactSorter) Less(i, j int) bool {
+	distI := xorDistance(cs.Contacts[i].ID, cs.TargetID)
+	distJ := xorDistance(cs.Contacts[j].ID, cs.TargetID)
+	return distI.Cmp(distJ) == -1
+}
+
+func (cs ContactSorter) Print() {
+	for _, v := range cs.Contacts {
+		fmt.Println("Contact: ", v, "Distance: ", xorDistance(v.ID, cs.TargetID))
+	}
 }
 
 func main() {
-	n1 := NewNodeId("testa")
-	n2 := NewNodeId("testb")
-	fmt.Println(n1.String())
-	fmt.Println(n2.String())
-	fmt.Println(xorDistance(n1, n2))
+	var contacts []Contact
+
+	for i := range 10 {
+		contacts = append(contacts,
+			Contact{ID: NewNodeId("test " + strconv.Itoa(i)), IP: net.IPv4zero, Port: 10000 + i})
+	}
+
+	contactTable := &ContactSorter{
+		TargetID: NewNodeId("test"),
+		Contacts: contacts,
+	}
+
+	contactTable.Print()
+
+	fmt.Println()
+
+	sort.Sort(contactTable)
+
+	contactTable.Print()
 }
