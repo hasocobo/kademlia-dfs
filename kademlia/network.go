29921f6ad97931311c345d1d6f4cfcac3c7b7946
Hasan Çoban <hasancoban13579@gmail.com>
2025-11-15 15:22:07 +0300

    Add simulation network implementation for Kademlia
    - Introduced a new SimNetwork struct to facilitate testing of core RPC calls without UDP complexity.
    - Implemented methods for pinging, finding nodes, finding values, and storing data within the simulation network.
    - Added synchronization mechanisms to ensure thread safety during network operations.

---
+++ kademlia/network.go
@@ -0,0 +1,81 @@
+package kademliadfs
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Network interface {
+	Ping(requster, recipient Contact) error
+	FindNode(requester, recipient Contact, targetID NodeId) ([]Contact, error)
+	FindValue(requester, recipient Contact, key NodeId) ([]byte, []Contact, error)
+	Store(requester, recipient Contact, key NodeId, value []byte) error
+}
+
+// Simulation network implements Network to test the core RPC calls without the complexity of UDP yet
+type SimNetwork struct {
+	Nodes map[NodeId]*Node
+	mu    sync.Mutex
+}
+
+func NewSimNetwork() *SimNetwork {
+	return &SimNetwork{
+		Nodes: make(map[NodeId]*Node),
+	}
+}
+
+func (sn *SimNetwork) Ping(requester, recipient Contact) error {
+	sn.mu.Lock()
+	defer sn.mu.Unlock()
+
+	if node, exists := sn.Nodes[recipient.ID]; exists {
+		node.HandlePing(requester)
+	}
+
+	return nil
+}
+
+func (sn *SimNetwork) FindNode(requester, recipient Contact, targetID NodeId) ([]Contact, error) {
+	sn.mu.Lock()
+	defer sn.mu.Unlock()
+
+	recipientNode, exists := sn.Nodes[recipient.ID]
+	if !exists {
+		return nil, fmt.Errorf("recipient node %v not found", recipient.ID)
+	}
+
+	return recipientNode.HandleFindNode(requester, targetID), nil
+}
+
+func (sn *SimNetwork) FindValue(requester, recipient Contact, key NodeId) ([]byte, []Contact, error) {
+	sn.mu.Lock()
+	defer sn.mu.Unlock()
+
+	recipientNode, exists := sn.Nodes[recipient.ID]
+	if !exists {
+		return nil, nil, fmt.Errorf("recipient node %v not found", recipient.ID)
+	}
+
+	value, contacts := recipientNode.HandleFindValue(requester, key)
+	return value, contacts, nil
+}
+
+func (sn *SimNetwork) Store(requester, recipient Contact, key NodeId, value []byte) error {
+	sn.mu.Lock()
+	defer sn.mu.Unlock()
+
+	recipientNode, exists := sn.Nodes[recipient.ID]
+	if !exists {
+		return fmt.Errorf("recipient node %v not found", recipient.ID)
+	}
+
+	recipientNode.HandleStore(requester, key, value)
+	return nil
+}
+
+func (sn *SimNetwork) Register(n *Node) {
+	sn.mu.Lock()
+	defer sn.mu.Unlock()
+
+	sn.Nodes[n.Self.ID] = n
+}
